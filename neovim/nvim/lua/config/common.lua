9c7844a417876773ecba74679d381dd2509cd493
Alexander Makeenkov <amakeenk@altlinux.org>
2025-07-16 06:20:19 +0300

    update neovim

--- neovim/nvim/lua/config/common.lua
+++ neovim/nvim/lua/config/common.lua
@@ -6,8 +6,6 @@ vim.opt.mousefocus = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.laststatus = 2
-vim.opt.tabstop = 4
-vim.opt.expandtab = true
 vim.opt.clipboard = "unnamedplus"
 
 vim.opt.colorcolumn = "80"
@@ -16,5 +14,7 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
+vim.opt.smarttab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true
+
