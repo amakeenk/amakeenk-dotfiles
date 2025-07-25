b49acfc5c57533603ce6cb50ef080ff392fd16f7
Alexander Makeenkov <amakeenk@altlinux.org>
2025-07-26 01:45:39 +0300

    update hyprland

--- hyprland/hypr/scripts/set_wallpaper.sh
+++ hyprland/hypr/scripts/set_wallpaper.sh
@@ -3,6 +3,12 @@
 WALLPAPER_PATH="${HOME}/.config/hypr/images/wallpaper.jpg"
 NEW_WALLPAPER_PATH="$(zenity --file-selection)"
 
-cp -f "${NEW_WALLPAPER_PATH}" "${WALLPAPER_PATH}"
+magick "${NEW_WALLPAPER_PATH}" "${WALLPAPER_PATH}"
 
-killall hyprpaper && hyprpaper &
+matugen image "${WALLPAPER_PATH}" --debug --show-colors --mode dark
+
+killall -9 hyprpaper; hyprpaper &
+killall -9 waybar; waybar &
+hyprctl reload
+
+notify-send --expire-time=1000 --print-id "Фоновое изображение изменено"
