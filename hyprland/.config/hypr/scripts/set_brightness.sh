e3e1f928161fb3d57a6b1fa2f7b745f5d9159dc7
Alexander Makeenkov <amakeenk@altlinux.org>
2025-07-31 20:56:06 +0300

    update

--- hyprland/.config/hypr/scripts/set_brightness.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-direction=$1
-ddcutil setvcp 10 $direction 5
-cur_value=$(ddcutil getvcp 10 --brief | cut -d' ' -f4)
-notify-send --expire-time=1000 --print-id "Яркость монитора изменена" "Текущая яркость: ${cur_value}"
